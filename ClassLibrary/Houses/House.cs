57cbbb96ee9b52eb26c22ab150ac6ea880db5fe1
ShevaMed <shevalenko87@gmail.com>
2023-04-12 13:50:03 +0300

    fourth commit

---
+++ ClassLibrary/Houses/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Houses
+{
+    public abstract class House
+    {
+        protected Image _img;
+        protected int _square;
+        protected int _cost;
+        
+        public abstract Image View();
+    }
+}
